35508775f9751e70d26294a0553fdb36b569f8d0
Ola Tuvesson <ola@iron.clickworkorange.net>
2023-05-15 21:30:46 +0100

    package_data includes images/

--- setup.py
+++ setup.py
@@ -5,19 +5,18 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='KivySightGlass',
-    version='0.0.1',
-    packages=['kivy_sight_glass'],
-    include_package_data=True,
+    name="KivySightGlass",
+    version="0.0.1",
+    packages=["kivy_sight_glass"],
     package_data={
-        "images": ["images/*.png"],
+        "kivy_sight_glass": ["images/*.png"],
     },
-    url='https://github.com/clickworkorange/KivySightGlass',
-    license='MIT',
-    author='clickworkorange',
-    author_email='info@clickworkorange.com',
-    description='A dynamic bar graph emulating the appearance of liquid inside a sight glass',
+    url="https://github.com/clickworkorange/KivySightGlass",
+    license="MIT",
+    author="clickworkorange",
+    author_email="info@clickworkorange.com",
+    description="A dynamic bar graph emulating the appearance of liquid inside a sight glass",
     install_requires=["Kivy", "KivyGradient"],
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type="text/markdown"
 )
